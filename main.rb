2777e2fe555b02562a4b904b044966f0195a95e0
Mari Imaizumi <mariimaizumi5@gmail.com>
2025-01-11 00:31:38 +0900

    Enable to run

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'nokogiri'
 require 'open-uri'
 require 'yaml'
 
@@ -9,47 +7,60 @@ require 'yaml'
 # ブログ記事から発表者名に基づいてタグを付けるスクリプト。
 # - 指定したURLの一覧をScrapingし、記事本文を取得する。
 # - 事前に用意した発表者名のリストを参照し、本文に含まれている発表者名をタグとして付与する。
+# - articles/YEAR/urls.yaml にURLのリストを記述することで、対象の記事を指定する。
 #
 # 使用例:
-#   ruby tag_rubykaigi_blog.rb
+#   ruby main.rb 2024
 
-speakers = URI.open('https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/refs/heads/master/2024/data/speakers.yml') do |f|
+SPEAKERS = URI.open('https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/refs/heads/master/2024/data/speakers.yml') do |f|
   YAML.safe_load(f)
 end
 
-presentations = URI.open('https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/refs/heads/master/2024/data/presentations.yml') do |f|
+PRESENTATIONS = URI.open('https://raw.githubusercontent.com/ruby-no-kai/rubykaigi-static/refs/heads/master/2024/data/presentations.yml') do |f|
   YAML.safe_load(f)
 end
 
 def download_blogs
+  unless File.exist?("articles/#{ARGV[0]}/urls.yaml")
+    puts "articles/#{ARGV[0]}/urls.yaml not found."
+    exit 1
+  end
+
+  File.open("articles/#{ARGV[0]}/urls.yaml", 'r') do |f|
+    urls = YAML.safe_load(f)
+    urls.each do |url|
+      download_file(url, "articles/#{ARGV[0]}")
+    end
+  end
+end
+
+def safe_filename_from_url(url_str)
+  url_str.gsub(/[^0-9A-Za-z._\-]/, '_')
 end
 
 def download_file(uri, path)
-  URI.open(uri) do |response|
-    IO.copy_stream(response, "#{path}/#{filename}")
-  end
+  safe_uri = safe_filename_from_url(uri)
+  return if File.exist?("#{path}/#{safe_uri}")
+
+    URI.open(uri) do |response|
+      IO.copy_stream(response, "#{path}/#{safe_uri}")
+    end
 end
 
 def fetch_blog_text(url)
-  html = URI.open(url) do |f|
+  html = File.open("articles/#{ARGV[0]}/#{safe_filename_from_url(url)}") do |f|
     f.read
   end
 
-  doc = Nokogiri::HTML(html)
-
-  article_element = doc.at_css('article')
-  return '' unless article_element
-
-  article_element.text
+  html
 end
-
 
 def extract_presenter_tags(text, presenters)
   tags = []
 
   presenters.each do |presenter|
     id = presenter.first
-    name = find_name_in_keynotes_or_speakers(presenters, id)
+    name = find_name_in_keynotes_or_speakers(SPEAKERS, id)
     title = presenter.last['title']
 
     regexp = Regexp.new("#{id}|#{name}|#{title}", Regexp::IGNORECASE)
@@ -71,13 +82,24 @@ def find_name_in_keynotes_or_speakers(data, target_id)
 end
 
 def main
-  BLOG_URLS.each do |url|
-    body_text = fetch_blog_text(url)
-    tags = extract_presenter_tags(body_text, presentations)
+  if ARGV.empty?
+    puts 'Please specify the year as an argument.'
+    puts 'Usage: ruby main.rb 2024'
+    exit 1
+  end
 
-    puts "URL: #{url}"
-    puts "Tags: #{tags.join(', ')}"
-    puts "-----"
+  download_blogs
+
+  File.open("articles/#{ARGV[0]}/urls.yaml", 'r') do |f|
+    urls = YAML.safe_load(f)
+    urls.each do |url|
+      body_text = fetch_blog_text(url)
+      tags = extract_presenter_tags(body_text, PRESENTATIONS)
+
+      puts "URL: #{url}"
+      puts "Tags: #{tags.join(', ')}"
+      puts "-----"
+    end
   end
 end
 
